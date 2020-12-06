1580431a932cbe5879c11e1db0323fcc5fd92323
amtesire <tesiregisele@gmail.com>
2020-12-06 14:15:44 +0200

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
+export SECRET_KEY='002'
+export DATABASE_URL='postgresql+psycopg2://wecode:1999@localhost/blog'
+export MAIL_USERNAME='giseletm99@gmail.com'
+export MAIL_PASSWORD='jesusislove'
 python3.6 manage.py server
